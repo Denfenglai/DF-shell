f4060512a86fff8528105550090793391c99b993
等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>
2023-05-01 06:15:06 +0000

    update shell/EC.sh.
    Signed-off-by: 等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>

--- shell/EC.sh
+++ shell/EC.sh
@@ -36,6 +36,7 @@ else
    rm -rf data/device.json
 fi
  echo -en "\033[32m 修复完成 回车返回\033[0m";read -p ""
+ exit
         ;;
         
         #装
@@ -45,6 +46,7 @@ fi
         echo "  https://github.com/icqqjs/icqq"
         echo "  不知道版本就不要用或者去官网找你要的"
         echo "=================================="
+        while true; do
         echo -e "请输入您要安装的icqq版本(格式：x.x):"
         echo -n "ICQQ@0.";read version
         if [[ "$version" == "n" ]]; then
@@ -53,9 +55,11 @@ fi
             cd ~/Yunzai-Bot
             pnpm i icqq@0.$version -w
             echo -en "\033[32m 修复完成 回车返回\033[0m";read -p ""
+        exit
         else
             echo -e "$R输入的格式不正确，请重新输入$N"
         fi
+        done
         ;;
         
     3)
@@ -77,6 +81,7 @@ fi
 #笨比Xx居然不自己cd！不过也好，这样适配还多一些
 cd ~/Yunzai-Bot
 bash <(curl -sL https://gitee.com/haanxuan/version/raw/master/version.sh)
+    break
     ;;
 
     4)
@@ -86,6 +91,7 @@ bash <(curl -sL https://gitee.com/haanxuan/version/raw/master/version.sh)
         echo -e "$G登录频繁被腾讯限制登录\n只能用手机号或扫码\n但目前没有手机号登录的框架\n扫码只支持手表还不一定成功\n所以237一律换号$N"
         echo
         echo -en "\033[32m 修复完成 回车返回\033[0m";read -p ""
+      exit
         ;;
         
     5)
@@ -103,23 +109,27 @@ fi
         echo "喵崽: rm -rf data/icqq"
         echo "你不清楚的话用脚本删就可以了"
         echo -en "\033[32m 修复完成 回车返回\033[0m";read -p ""
+       exit
         ;;
         
         6)
             echo -e "$R目前238只在手表账密登录出现\n手表只支持扫码登录\n使用手表协议请扫码登录\n需要账密登录推荐使用iPad$N"
             echo -en "\033[32m 修复完成 回车返回\033[0m";read -p ""
+            exit
             ;;
             
         oicq)
             echo "咕咕~"
             echo "刨个坑"
             echo -en "\033[32m 未完待续 回车返回\033[0m";read -p ""
+          exit
             ;;
             
         icqq)
             echo "咕咕~"
             echo "刨个坑"
             echo -en "\033[32m 未完待续 回车返回\033[0m";read -p ""
+          exit
             ;;
             
         0)
