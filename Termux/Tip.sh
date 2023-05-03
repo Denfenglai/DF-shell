51144bb585b3a68b16cc150cfa1c2cade539c72f
等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>
2023-05-03 04:36:28 +0000

    ❛‿˂̵✧
    Signed-off-by: 等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>

---
+++ Termux/Tip.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -e "/usr/local/bin/d" ]; then
+clear
+  sed -i '/bash Tip.sh/d' /root/.bashrc
+  source ~/.bashrc
+  d
+  rm -rf Tip.sh
+  exit
+else
+  :
+fi 
+
+clear
+echo -e "\e[1;33m  更新软件包\e[0m"
+apt update
+
+clear
+echo -e "\e[1;33m  安装Curl\e[0m"
+apt install curl -y
+
+clear
+echo -e "\e[1;36m  少女祈祷中..\e[0m"
+bash <(curl -sL https://gitee.com/Wind-is-so-strong/yz/raw/master/install.sh)
