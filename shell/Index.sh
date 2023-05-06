c736b38fcfbf5f83623c2eb8a191403d160a410f
等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>
2023-05-06 02:32:41 +0000

    update shell/Index.sh.
    Signed-off-by: 等风来 <12472837+Wind-is-so-strong@user.noreply.gitee.com>

--- shell/Index.sh
+++ shell/Index.sh
@@ -269,6 +269,7 @@ fi
     if [ ! -f "/usr/local/bin/d" ]; then
     clear
         wget -O /usr/local/bin/d https://gitee.com/Wind-is-so-strong/yz/raw/master/shell/Index.sh >> wget.log 2>&1
+        chmod +x /usr/local/bin/d
         # 显示下载进度条
         {
             for ((i = 0 ; i <= 100 ; i+=1)); do
